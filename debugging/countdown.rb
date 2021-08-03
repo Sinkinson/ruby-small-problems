ccf347b63bbabf10f7ec03e8f89eea63a264c62b
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2021-08-03 09:53:14 +0200

    debugging problems

--- debugging/countdown.rb
+++ debugging/countdown.rb
@@ -1,19 +1,33 @@
 # Our countdown to launch isn't behaving as expected. Why? Change the code so that our program successfully counts down from 10 to 1.
 
-# def decrease(counter)
-#   counter -= 1
-# end
+def decrease(counter)
+  counter -= 1
+end
 
-# counter = 10
+counter = 10
 
-# 10.times do
-#   puts counter
-#   decrease(counter)
-# end
+10.times do
+  puts counter
+  decrease(counter)
+end
 
-# puts 'LAUNCH!'
+puts 'LAUNCH!'
 
-# solution
+# solution 1
+def decrease(counter)
+  counter - 1
+end
+
+counter = 10
+
+10.times do
+  puts counter
+  counter = decrease(counter)
+end
+
+puts 'LAUNCH!'
+
+# solution 2
 counter = 10
 
 10.times do
