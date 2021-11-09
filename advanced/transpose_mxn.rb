e1e12328ba3d17119297f07dbc6d4a58db5f21bc
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2021-11-09 13:35:14 +0100

    [+] exercise

---
+++ advanced/transpose_mxn.rb
@@ -0,0 +1,28 @@
+# Modify your transpose method from the transpose.rb exercise so it works with any matrix with at least 1 row and 1 column.
+
+def transpose(matrix)
+  result = Array.new
+  matrix.first.size.times { result << [] }
+  matrix.each do |matrix|
+    matrix.each_with_index { |value, idx| result[idx] << value }
+  end
+  result
+end
+
+p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+   [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose([[1]]) == [[1]]
+
+# option 2
+def transpose(matrix)
+  result = []
+  number_of_rows = matrix.size
+  number_of_columns = matrix.first.size
+  (0...number_of_columns).each do |column_index|
+    new_row = (0...number_of_rows).map { |row_index| matrix[row_index][column_index] }
+    result << new_row
+  end
+  result
+end
